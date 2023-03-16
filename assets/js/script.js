237ccedb2113bea87ae00cc9d20192ab3e8bf47f
cgonz <christoph.gonz@gmail.com>
2023-03-16 03:08:18 -0700

    added questions outline + added ids to use

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+var questions = [
+  {
+    title: "",
+    choices: ["", ""],
+    correctAnswer: "",
+  },
+  {
+    title: "",
+    choices: ["", ""],
+    correctAnswer: "",
+  },
+  {
+    title: "",
+    choices: ["", ""],
+    correctAnswer: "",
+  },
+  {
+    title: "",
+    choices: ["", ""],
+    correctAnswer: "",
+  },
+  {
+    title: "",
+    choices: ["", ""],
+    correctAnswer: "",
+  },
+  {
+    title: "",
+    choices: ["", ""],
+    correctAnswer: "",
+  },
+  {
+    title: "",
+    choices: ["", ""],
+    correctAnswer: "",
+  },
+  {},
+];
+
+var questionNo = 0;
